2af68c3d6d868ae1844f7bbe07365c7b1088e9ff
junhowoo <junho.woo@musinsa.com>
2024-01-26 16:39:22 +0900

    feat: [2-cache] 유효기간 없는 캐시 값 변경되어야 할때 테스트

--- api-server/index.js
+++ api-server/index.js
@@ -9,16 +9,22 @@ function simpleProcess() {
     return "simple"
 }
 
-app.get("/simple-api", (req, res) => {
-    const result = simpleProcess()
-    return res.json({
-        "data": "simple",
-    })
-})
+const heavyProcessResults = ["heavy1", "heavy2", "heavy3", "heavy4", "heavy5", "heavy6", "heavy7", "heavy8", "heavy9", "heavy10"]
+let currentPointer = 0
+let heavyProcessMustBe = heavyProcessResults[currentPointer]
 
-
+setInterval(() => {
+    // change heavyProocessMustBe every 3 seconds by currentPointer + 1
+    currentPointer = currentPointer + 1
+    if (currentPointer > 9) {
+        currentPointer = 0
+    }
+    heavyProcessMustBe = heavyProcessResults[currentPointer]
+    console.log("heavyProcessMustBe : ", heavyProcessMustBe)
+}, 3000)
 async function heavyProcess() {
     // look a side pattern
+    const heavyResult = heavyProcessResults[currentPointer]
     const resultFromCache = await redisConnect.get("heavy")
 
     if (resultFromCache) {
@@ -28,24 +34,39 @@ async function heavyProcess() {
 
     const result = new Promise((resolve, reject) => {
         setTimeout(() => {
-            redisConnect.set("heavy", "heavy")
+            redisConnect.set("heavy", heavyResult)
             console.log("No Cache")
-            resolve("heavy")
+            resolve(heavyResult)
         }, 2000)
     })
 
     return result
 }
 
-app.get("/heavy-api", async (req, res) => {
-    const startTime = new Date().getTime()
-    const result = await heavyProcess()
-    const endTime = new Date().getTime()
-    console.log("Time : ", endTime - startTime)
-
+app.get("/simple-api", (req, res) => {
+    const result = simpleProcess()
     return res.json({
-        "data": result,
+        "data": "simple",
     })
+})
+
+app.get("/heavy-api", async (req, res) => {
+    try {
+        const startTime = new Date().getTime()
+        const result = await heavyProcess()
+        const endTime = new Date().getTime()
+        console.log("Time : ", endTime - startTime)
+        if (result != heavyProcessResults[currentPointer]) {
+            throw new Error("갱신 안됨")
+        }
+        return res.json({
+            "data": result,
+        })
+    }catch (e) {
+        return res.status(500).json({
+            message: e.message
+        })
+    }
 })
 
 app.listen(3000, () => {
